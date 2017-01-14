b2291f5061b45d7bacc1d88c97f78596bdb10df1
andres mariscal <mariizcal@gmail.com>
2017-01-14 17:18:16 -0600

    initial commit

---
+++ app/src/main/java/com/mariizcal/tictactoe/BoardActivity.kt
@@ -0,0 +1,12 @@
+package com.mariizcal.tictactoe
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BoardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_board)
+    }
+}
